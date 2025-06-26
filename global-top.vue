139f421a1916c1abe9393b28d702c6dca9b02d91
kirill.chalov <kirill.chalov@espressif.com>
2025-06-26 19:48:57 +0800

    feat: add espressif logo on slides

---
+++ global-top.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer
+    style="
+      position: fixed;
+      top: 1rem;
+      right: 2rem;
+      width: 200px;
+      opacity: 0.8;
+      pointer-events: none;
+      z-index: 9999;
+    "
+  >
+    <img src="/logos/DevCon25_Logo_Horizontal_White.svg" alt="Espressif Logo" style="width: 100%;" />
+  </footer>
+</template>
